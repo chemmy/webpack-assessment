6354ccd89b9c36e7c7db07a70237880744f4b883
Chemmy Tutor <chemmy0430@gmail.com>
2020-09-07 16:47:58 +0800

    Add webpackconfig and babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,46 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./public/index.html",
+  filename: "./index.html",
+});
+
+const baseConfig = {
+  entry: "./src/js/index.js",
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.bundle.js",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "src/js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+};
+
+module.exports = function (env) {
+  const isDev = process.env.NODE_ENV === "development";
+  console.log(`${isDev ? "Development" : "Production"} build`);
+
+  if (isDev) {
+    baseConfig.plugins.push(
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin()
+    );
+  }
+
+  return baseConfig;
+};
